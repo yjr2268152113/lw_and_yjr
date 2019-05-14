8885f55cba622eaf4dc6991df483f989068435e0
yjr2268152113 <2268152113@qq.com>
2019-05-14 19:35:34 +0800

    5.14

---
+++ wxapp/lottery/pages/index/index.js
@@ -0,0 +1,10 @@
+const app=getApp();
+page({
+  data:{
+    color:[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,],
+    images: ['/images/item.png', '/images/item1.png', '/images/item.png', '/images/item1.png', '/images/item.png', '/images/item1.png', '/images/item.png', '/images/item1.png', '/images/item.png'],
+    btnconfirm: '/images/dianjichoujiang.png',
+
+  },
+
+})

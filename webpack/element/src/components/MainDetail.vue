10eb162808368ba6fed80cb979b8a61d9ca5e633
yjr2268152113 <2268152113@qq.com>
2019-05-30 19:10:15 +0800

    5.30

---
+++ webpack/element/src/components/MainDetail.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="maindetail">这是默认首页</div>
+</template>
+<script>
+export default {
+    name:'MainDatail',
+    data(){
+        return{
+            message:'这是默认首页'
+        }
+    }
+}
+</script>
+

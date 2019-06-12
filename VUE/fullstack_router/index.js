3439d2b777016c0fda9c23fb79afe728b75966a7
yjr2268152113 <2268152113@qq.com>
2019-06-12 23:42:00 +0800

    6.12

---
+++ VUE/fullstack_router/index.js
@@ -0,0 +1,21 @@
+//后端开发  /api/shops
+const http=require('http')
+const fs=require('fs')
+
+http.createServer(function(req,res){
+    var filepath='.'+req.url;
+    if(filepath==='./'){
+        filepath='./index.html'
+    }
+    readFile(filepath,res)
+}).listen(3000)
+function readFile(path,res){
+    fs.readFile(path,'utf-8',function(err,data){
+        if(err){
+            readFile('./index.html',res)
+        }else{
+            res.write(data);
+            res.end()
+        }
+    })
+}
---
+++ VUE/fullstack_router/readme.md
@@ -0,0 +1,6 @@
+路由
+前端路由  统管 a 超链接     不要白屏   onhashChange
+pushState url 改变的 hash #home path/home/shop
+后端路由
+后端路由 启用 web-server  80    /   启动根路由
+index.html

3aee2139e7846bf3273eb1234cd2c9e7903b042e
yjr2268152113 <2268152113@qq.com>
2019-07-07 22:00:06 +0800

    7.7

--- VUE/vue-components/src/components/com/button.vue
+++ VUE/vue-components/src/components/com/button.vue
@@ -10,6 +10,7 @@ function oneOf(value,validList){
     for(let i=0;i<validList.length;i++){
         if(value===validList[i]){
             return true
+            
         }
     }
     return false
